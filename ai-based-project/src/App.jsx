e2d27ca7d41b14d82c254fe25dc2520a212dd411
pragati-2024 <bpragati21@gmail.com>
2025-07-12 11:55:16 +0530

    removed unwanted files

--- ai-based-project/src/App.jsx
+++ ai-based-project/src/App.jsx
@@ -1,20 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './home/Page';
-import About from './about-us/Page';
-import Contact from './contact-us/Page';
-import Tips from './interview-tips/Page';
-import Login from './login/Page';
-import NotFound from './error/Page';
-import Footer from './footer/Page';
-import Navbar from './Navbar/Page';
-
-
-
-
-
-
-
 const App = () => {
   return (
     <div>
