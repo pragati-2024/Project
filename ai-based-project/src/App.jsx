d4ff822c22b867eedee0a3992764177d38f08e20
pragati-2024 <bpragati21@gmail.com>
2025-07-12 18:31:21 +0530

    404-error-empty-file-added

--- ai-based-project/src/App.jsx
+++ ai-based-project/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header.jsx";
 import Footer from "./components/Footer.jsx";
 import Login from './components/Login.jsx';
 import ContactUs from './components/ContactUs.jsx';
+import Eror404 from './components/Eror404.jsx'; 
 
 import "./App.css";
 
@@ -17,6 +18,7 @@ function App() {
       <InterviewTips />
       <Login />
       <ContactUs />
+      <Eror404 />
     </div>
   );
 }
---
+++ ai-based-project/src/components/Eror404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Eror404 = () => {
+  return (
+    <div>Eror404</div>
+  )
+}
+
+export default Eror404
